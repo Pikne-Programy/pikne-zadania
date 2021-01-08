a5112355483d91f4c937c5027aaba5957f06e575
Marcin Zepp <nircek-2103@protonmail.com>
2021-01-08 20:55:55 +0100

    docker watchtower test

--- server.ts
+++ server.ts
@@ -2,6 +2,23 @@ import { Application, send } from "./deps.ts";
 
 const app = new Application();
 
+// src: https://github.com/oakserver/oak
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
 app.use(async (context) => {
   await send(context, context.request.url.pathname, {
     root: `${Deno.cwd()}/frontend`,
