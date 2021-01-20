bcbf03309f2623e318d07fc2c280d46f71f633fe
Marcin Zepp <nircek-2103@protonmail.com>
2021-01-21 00:14:55 +0100

    Add more logging in request handling

--- deps.ts
+++ deps.ts
@@ -1,7 +1,9 @@
 export {
   Application,
+  HttpError,
   Router,
   send,
+  Status,
 } from "https://deno.land/x/oak@v6.4.1/mod.ts";
 export type { RouterContext } from "https://deno.land/x/oak@v6.4.1/mod.ts";
 export { parse, parseAll } from "https://deno.land/std@0.83.0/encoding/yaml.ts";
--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Application, send } from "./deps.ts";
+import { Application, HttpError, send, Status } from "./deps.ts";
 import router from "./router.ts";
 
 interface State {
@@ -6,6 +6,37 @@ interface State {
 }
 
 const app = new Application<State>();
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  console.log(
+    `Listening on: ${secure ? "https://" : "http://"}${hostname ??
+      "localhost"}:${port}`,
+  );
+});
+
+app.addEventListener("error", (e) => {
+  console.log(e.error);
+});
+
+const basicHTMLTemplate = (msg: string) =>
+  `<!DOCTYPE html><html><body><h1>${msg}</h1></body></html>`;
+
+app.use(async (context, next) => {
+  try {
+    await next();
+  } catch (e) {
+    if (e instanceof HttpError) {
+      context.response.status = e.status;
+      context.response.body = basicHTMLTemplate(
+        `${e.status} ${e.expose ? e.message : Status[e.status]}`,
+      );
+    } else if (e instanceof Error) {
+      context.response.status = 500;
+      context.response.body = basicHTMLTemplate("500 Internal Server Error");
+      console.error(e.message, e.stack);
+    }
+  }
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
