d1934072e32cd7116a5a5eec529759136a65348d
Michał Szymocha <szymocha.michal@gmail.com>
2021-03-06 15:33:49 +0100

    Add v0.2 api endpoints

--- backend/router.ts
+++ backend/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "./deps.ts";
-import exercisesController from "./controllers/exercisesController.ts";
 import { seed } from "./middleware/seedMiddleware.ts";
+import exercisesController from "./controllers/exercisesController.ts";
+import authController from "./controllers/authController.ts";
 
 const router = new Router();
 
@@ -19,7 +20,60 @@ router
     "/api/public/:subject/:id",
     seed,
     exercisesController.check.bind(exercisesController),
+  )
+  .post(
+    "/api/register",
+    authController.register.bind(authController),
+  )
+  .post(
+    "/api/login",
+    authController.login.bind(authController),
+  )
+  .post(
+    "/api/login",
+    authController.logout.bind(authController),
+  )
+  .get(
+    "/api/account",
+    authController.getAccount.bind(authController),
+  )
+  .get(
+    "/api/teams",
+    authController.getTeams.bind(authController),
+  )
+  .post(
+    "/api/teams",
+    authController.createTeam.bind(authController),
+  )
+  .post(
+    "/api/teams/:id/open",
+    authController.openReg.bind(authController),
+  )
+  .post(
+    "/api/teams/:id/close",
+    authController.closeReg.bind(authController),
+  )
+  .get(
+    "/api/teams/:id",
+    authController.teamInfo.bind(authController),
+  )
+  .delete(
+    "/api/teams/:id/:userid",
+    authController.deleteUser.bind(authController),
+  )
+  .post(
+    "/api/teams/:id/:userid",
+    authController.addUser.bind(authController),
+  )
+  .post(
+    "/api/teams/:id",
+    authController.setTeamName.bind(authController),
+  )
+  .post(
+    "/api/root/teams/:id",
+    authController.changeAssignee.bind(authController),
   );
+
 // TODO: images in exercises
 
 export default router;
