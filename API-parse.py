692105d71d2dec3a748bd4759c3c1a8aea8490f0
Marcin Zepp <nircek-2103@protonmail.com>
2022-01-04 23:04:43 +0100

    Add the notes directory and comments in the API generation tool
    Also reformat API.adoc

--- API-parse.py
+++ API-parse.py
@@ -6,36 +6,58 @@
 import sys
 import re
 
-with open('gen.txt', 'r') as f:
+assert len(sys.argv) == 2
+with open(sys.argv[1], 'r') as f:
     x = f.read()
 
-offset = x.find('@'*37)
-offset2 = x.find('@'*37, offset+1)
-print(x[:offset])
-content = x[offset+38:offset2]
+offset1 = x.find('#'*37+'\n')
+offset2 = x.find('@'*37+'\n')
+offset3 = x.find('@'*37+'\n', offset2+1)
+
+print(x[:offset1-1])
+Notes = x[offset1+38:offset2]
+notesD = {}
+for match in re.finditer(r'^// (\S*)\n([\s\S]*?\n)(?=\n//|\Z)', Notes, re.M):
+    assert match.group(1) not in notesD, f'`{match.group(1)} already in Notes'
+    notesD[match.group(1)] = match.group(2)
+Content = x[offset2+38:offset3]
+print('== Endpoints\n')
 n = 0
-for match in re.finditer(r'^(?:(?P<section># [^\n]*)|(?P<content>\\\^ (.*)[\s\S]*?)(?=(?:^# [^\n]*|\Z|^\\\^ (.*))))', content, re.M):
+for match in re.finditer(r'^(?:(?P<section># [^\n]*)|(?P<content>\\\^ (.*)[\s\S]*?)(?=(?:^# [^\n]*|\Z|^\\\^ (.*))))', Content, re.M):
     d = match.groupdict()
     if d['section'] is not None:
         print(f"\n=== {d['section'][2:]}")
         continue
     n += 1
     raw = d['content']
-    d = re.search(r'\\\^ (?P<endpoint>[^\n]*?) - (?P<desc>[^\n]*?)\n\\\$\n(?P<used>[\s\S]*?\n)\\%\n(?P<codes>[\s\S]*?)\\>\n(?P<request>[\s\S]*?)\\<\n(?P<response>[\s\S]*?)\\!(?P<notes>[\s\S]*)', raw).groupdict()
+    d = re.search(r'\\\^ (?P<endpoint>[^\n]*?) - (?P<desc>[^\n]*?)\n\\#\n(?P<comment>[\s\S]*?)\\\$\n(?P<used>[\s\S]*?\n)\\%\n(?P<codes>[\s\S]*?)\\>\n(?P<request>[\s\S]*?)\\<\n(?P<response>[\s\S]*?)\\!\n(?P<notes>[\s\S]*)', raw).groupdict()
     endpoint = d['endpoint']
     desc = d['desc']
     tag = '_'.join(re.split(r'(?: |\/)+', d['endpoint'])).replace(':', '.')
+    comment = '\n' + d['comment'] if d['comment'] else ''
     used = d['used']
     codes = d['codes']
     request = '\n'+d['request'] if d['request'] else ''
     response = '\n'+d['response'] if d['response'] else ''
-    notes = d['notes']
+    notes = ''
+    while d['notes'] and d['notes'][-1] == '\n':
+        d['notes'] = d['notes'][:-1]
+    for line in d['notes'].split('\n') if d['notes'] else []:
+        if re.match(r'^TODO: ', line):
+            notes += f'// {line}\n'
+        else:
+            if line in notesD:
+                notes += f'// {line}\n{notesD[line]}'
+            else:
+                print(f'WARN: `{line}` not in Notes dict', file=sys.stderr)
+    if notes:
+        notes = '\n' + notes
     endl = '\n'
     print(f'''
 .`{endpoint}` - {desc} [[{tag}]]
 [%collapsible]
 ====
-
+{comment}
 .It can be used by
 {used}
 .Special status codes
@@ -48,6 +70,18 @@ for match in re.finditer(r'^(?:(?P<section># [^\n]*)|(?P<content>\\\^ (.*)[\s\S]
 {notes}
 ====''')
 
+print(f'''
+////
 
-print(x[offset2+38:], end='')
+== Notes database
+
+[%collapsible]
+====
+
+{Notes}
+====
+
+////''')
+
+print(x[offset3+38:], end='')
 print(n, file=sys.stderr)
--- API-unparse.py
+++ API-unparse.py
@@ -6,27 +6,33 @@
 import sys
 import re
 
-with open('API.adoc', 'r') as f:
+assert len(sys.argv) == 2
+with open(sys.argv[1], 'r') as f:
     x = f.read()
 
-offset = next(re.finditer(r'\n=+\s+Endpoints\n', x)).end()
-print(x[:offset], end='')
+endpointsM = re.search(r'^=+\s+Endpoints\n', x, re.M)
+notesM = re.search(r'\n=+\s+Notes database\n[\s\S]*?\n={4}\n*([\s\S]*)\n\n={4}\n\n/{4}\n', x)
+print(x[:endpointsM.start()], end='')
+print('#'*37)
+print(notesM.group(1))
 print('@'*37)
 n = 0
-for section in re.finditer(r'^={3}\s+(\S.*)\n([\s\S]*?)(?=^={3}\s+(\S.*)\n|^={2}\s+\S)', x, re.M):
+for section in re.finditer(r'^={3}\s+(\S.*)\n([\s\S]*?)(?=^={3}\s+(\S.*)\n|^={2}\s+\S)', x[endpointsM.start():notesM.end()], re.M):
     name, content, *_ = section.groups()
     print(f'# {name}')
-    for endpoint in re.finditer(r'\.`(?P<endpoint>[^`\n]*)` - (?P<desc>[^[\n]*) \[\[.*\]\][\s\S]*?^\.It can be used by\n(?P<used>(?:[^\.\n][^\n]*\n)*)\n\.Special status codes\n(?P<codes>(?:[^\.\n][^\n]*\n)*)\n\.Request\n{1,2}(?P<request>(?:(?:[^\.\n][^\n]*|)\n)*)\n\.Response\n{1,2}(?P<response>(?:(?:[^\.\n][^\n]*|)\n)*)\n\.Notes\n\n(?P<notes>[\s\S]*?)(?=\n====)', content, re.M):
+    for endpoint in re.finditer(r'\.`(?P<endpoint>[^`\n]*)` - (?P<desc>[^[\n]*) \[\[.*\]\][^=]*====\n*(?P<comment>(?:[^\.\n][^\n]*\n)*)\n\.It can be used by\n(?P<used>(?:[^\.\n][^\n]*\n)*)\n\.Special status codes\n(?P<codes>(?:[^\.\n][^\n]*\n)*)\n\.Request\n{1,2}(?P<request>(?:(?:[^\.\n][^\n]*|)\n)*)\n\.Response\n{1,2}(?P<response>(?:(?:[^\.\n][^\n]*|)\n)*)\n\.Notes\n\n(?P<notes>[\s\S]*?)(?=====)', content, re.M):
         d = endpoint.groupdict()
         n += 1
         print('\\^', f"{d['endpoint']} - {d['desc']}")
+        print('\\#', d['comment'], sep='\n', end='')
         print('\\$', d['used'], sep='\n', end='')
         print('\\%', d['codes'], sep='\n', end='')
         print('\\>', d['request'], sep='\n', end='')
         print('\\<', d['response'], sep='\n', end='')
-        print('\\!', d['notes'], sep='\n', end='')
+        notes = list(map(lambda x: x[0] if x[2] == '' else f"TODO: {x[2]}", re.findall(r'^// (?:(\S*)\n([\s\S]*?\n)|TODO: (.*))(?=\n*(//|\Z))', d['notes'], re.M)))
+        notes += [''] if notes else []
+        print('\\!', '\n'.join(notes), sep='\n', end='')
 
-offset = next(re.finditer(r'\n=+\s+Used', x)).start()
 print('@'*37)
-print(x[offset:], end='')
+print(x[notesM.end():], end='')
 print(n, file=sys.stderr)
